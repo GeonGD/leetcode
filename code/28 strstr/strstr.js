64502fd6233a73be30bbc85d371419e21d88526c
hehuan <1025005090@qq.com>
2018-03-11 17:42:13 +0800

    '28over'

---
+++ code/28 strstr/strstr.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ * @author HeHuan
+ */
+var strStr = function (haystack, needle) {
+    return haystack.indexOf(needle)
+};
