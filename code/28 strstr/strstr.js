d0e2777cef65cd402e4f9b4db64d28c70fb72a9b
郭东升 <dongsheng0830@gmail.com>
2020-03-07 15:51:46 +0800

    初始化develop分支，添加jest eslint

--- code/28 strstr/strstr.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- * @author HeHuan
- */
-var strStr = function (haystack, needle) {
-    return haystack.indexOf(needle)
-};
--- code/9 isPalindrome/isPalindrome.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-    return x.toString() == x.toString().split('').reverse().join('');
-};
