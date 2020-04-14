e4c18be689ed029017f2cc8521d735290f0f6c82
GeonGD <984227843@qq.com>
2020-04-14 11:59:43 +0800

    增加addTwoNumbers题解

---
+++ code/LinkList/addTwoNumbers.js
@@ -0,0 +1,45 @@
+/**
+ * @description 两数相加
+ */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+const addTwoNumbers = (l1, l2) => {
+    let arr1 = [];
+    let arr2 = [];
+    while (l1) {
+        arr1.push(l1.val);
+        l1 = l1.next;
+    }
+    while (l2) {
+        arr2.push(l2.val);
+        l2 = l2.next;
+    }
+    let res = [0];
+    for (let i = arr1.length - 1, j = arr2.length - 1, k = 0; i >= 0 || j >= 0; k++, i--, j--) {
+        let num1 = i >= 0 ? arr1[i] : 0;
+        let num2 = j >= 0 ? arr2[j] : 0;
+        console.log(num1, num2, res[k]);
+        let a = num1 + num2 + res[k];
+
+        res[k] = a % 10;
+        res[k + 1] = a / 10 | 0;
+    }
+    let start = new ListNode(res[res.length - 2]);
+    let node1 = start;
+    for (let i = res.length - 3; i >= 0; i--) {
+        let node = new ListNode(res[i]);
+        node1.next = node;
+        node1 = node1.next;
+    }
+    if (res[res.length - 1] !== 0) {
+        let node2 = new ListNode(res[res.length - 1]);
+        node2.next = start;
+        return node2;
+    }
+    return start;
+};
+
+export default addTwoNumbers;
